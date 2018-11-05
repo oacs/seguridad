4306e2f660dc46256af88e8cf295c29c4ecbffe2
María Eugenia Tirado <mariatiperez@gmail.com>
2018-11-04 21:10:15 -0400

    descifrado

--- src/app/descifrado/descifrado.component.ts
+++ src/app/descifrado/descifrado.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { abcdario } from '../app.component';
 
 @Component({
   selector: 'app-descifrado',
@@ -7,9 +8,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DescifradoComponent implements OnInit {
 
+  public texto = [];
+  public cifrado: string;
+  public alfabeto: Map<string, string>;
   constructor() { }
 
   ngOnInit() {
+    this.alfabeto = new Map<string, string>();
+    this.alfabeto.set(' ', ' ');
+    this.cifrado = '';
+    this.random();
+    console.log(this.alfabeto);
+    this.cifrado = 'Hola mundo';
+    console.log(this.cifrado);
+    this.cifrado = this.cifrado.toLowerCase();
+    this.descifrar();
+    console.log(this.cifrado);
+  }
+
+  public descifrar() {
+    for (let index = 0; index < this.cifrado.length; index++) {
+      this.texto[index] = ((this.cifrado.charAt(index)) );
+    }
+  }
+
+  public random() {
+    const aux = abcdario.slice();
+    let j, x, i;
+    for (i = aux.length - 1; i > 0; i--) {
+        j = Math.floor(Math.random() * (i + 1));
+        x = aux[i];
+        aux[i] = aux[j];
+        aux[j] = x;
+    }
+    abcdario.forEach(element => {
+      this.alfabeto.set(aux.pop(), element);
+    });
+
   }
 
 }
