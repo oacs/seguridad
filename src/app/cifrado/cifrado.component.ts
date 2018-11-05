bb55d2b0c6bb77b23ac37ac1b4263ca7cd55dc9f
María Eugenia Tirado <mariatiperez@gmail.com>
2018-11-04 20:44:48 -0400

    cifrado

--- src/app/cifrado/cifrado.component.ts
+++ src/app/cifrado/cifrado.component.ts
@@ -7,9 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CifradoComponent implements OnInit {
 
+  public texto: string;
+  public cifrado: string;
+  public alfabeto: Map<string, string>;
   constructor() { }
 
   ngOnInit() {
+  }
+
+  public cifrar(){
+    for (let index = 0; index < this.texto.length; index++) {
+      this.cifrado.concat(this.alfabeto.get(this.texto.charAt(index)) );
+    }
   }
 
 }
