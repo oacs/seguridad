0a3e57f3412810f0eb15db009dbbebac4051f098
María Eugenia Tirado <mariatiperez@gmail.com>
2018-11-04 20:44:38 -0400

    abcdario

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+export const abcdario = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
