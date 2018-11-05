eba3ba88ef18124836b6dc0529fcf12923767a42
Unknown <joseanzacarias@gmail.com>
2018-11-04 23:14:05 -0400

    Problema en platilla de cifrado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { OpcionesComponent } from './opciones/opciones.component';
+import { CifradoComponent } from './cifrado/cifrado.component';
 
 const routes: Routes = [
-  {path: '', component: OpcionesComponent}
+  {path: '', component: OpcionesComponent},
+  {path: 'cifrado', component: CifradoComponent}
+
 ];
 
 @NgModule({
